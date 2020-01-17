c3cd7b9694249e9027450c91d3b853ab1f498a8a
din_alt@poczta.onet.pl <din_alt@poczta.onet.pl>
2020-01-17 18:52:54 +0100

    Added menus.

--- Notatnik.NET/MainWindow.xaml.cs
+++ Notatnik.NET/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace Notatnik.NET
 {
@@ -20,9 +10,86 @@ namespace Notatnik.NET
     /// </summary>
     public partial class MainWindow : Window
     {
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private string filePath = null;
+        private bool isTextChanged = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Wybierz plik tekstowy.";
+            openFileDialog.DefaultExt = "txt";
+            openFileDialog.Filter = "Pliki tekstowe (*.txt) | *.txt | Pliki XML (*.xml) | *.xml | Pliki źródłowe (*.cs) | *.cs | " +
+                "Wszystkie pliki (*.*) | *.*";
+            openFileDialog.FilterIndex = 1;
+
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Zapisz plik tekstowy";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.Filter = openFileDialog.Filter;
+            saveFileDialog.FilterIndex = 1;
+        }
+
+        private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(filePath))
+            {
+                openFileDialog.InitialDirectory = Path.GetDirectoryName(filePath);
+                openFileDialog.FileName = Path.GetFileName(filePath);
+            }
+            bool? result = openFileDialog.ShowDialog();
+            if(result.HasValue && result.Value)
+            {
+                filePath = openFileDialog.FileName;
+                textBox.Text = File.ReadAllText(filePath);
+                statusBarText.Text = Path.GetFileName(filePath);
+            }
+        }
+
+        private void MenuItem_ZapiszJako_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(filePath))
+            {
+                saveFileDialog.InitialDirectory = Path.GetDirectoryName(filePath);
+                saveFileDialog.FileName = Path.GetFileName(filePath);
+            }
+
+            bool? result = saveFileDialog.ShowDialog();
+            if(result.HasValue && result.Value)
+            {
+                filePath = saveFileDialog.FileName;
+                File.WriteAllText(filePath, textBox.Text);
+                statusBarText.Text = Path.GetFileName(filePath);
+                isTextChanged = false;
+            }
+           
+        }
+
+        private void MenuItem_Zapisz_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(filePath))
+            {
+                File.WriteAllText(filePath, textBox.Text);
+                isTextChanged = false;
+            }
+            else
+            {
+                MenuItem_ZapiszJako_Click(sender, e);
+            }
+            
+        }
+
+        private void MenuItem_Zamknij_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            isTextChanged = true;
         }
     }
 }
