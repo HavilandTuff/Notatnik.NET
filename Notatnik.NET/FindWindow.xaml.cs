f1153a93a9adac34d17f85909849a8f2a766c3ef
din_alt@poczta.onet.pl <din_alt@poczta.onet.pl>
2020-02-26 19:02:45 +0100

    Search function works.

--- Notatnik.NET/FindWindow.xaml.cs
+++ Notatnik.NET/FindWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace Notatnik.NET
     public partial class FindWindow : Window
     {
         string textBuffer = "";
+        int index = 0;
+        bool stringFound = false;
         public FindWindow(string text)
         {
             InitializeComponent();
@@ -38,6 +40,35 @@ namespace Notatnik.NET
         private void FindButton_Click(object sender, RoutedEventArgs e)
         {
 
+
+            if (!string.IsNullOrEmpty(SearchField.Text))
+            {
+                index = textBuffer.IndexOf(SearchField.Text, index);
+                
+                if (index > -1)
+                {
+                    Window parentWindow = Application.Current.MainWindow;
+                    ((MainWindow)parentWindow).textBox.CaretIndex = index;
+                    ((MainWindow)parentWindow).textBox.Focus();
+                    ((MainWindow)parentWindow).textBox.Select(index, SearchField.Text.Length);
+                    index++;
+                    stringFound = true;
+                }
+                else if (index == -1 && stringFound == false)
+                {
+                    MessageBox.Show("String not found!", "",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Exclamation);
+                    index = 0;
+                }
+                else if (index == -1 && stringFound == true)
+                {
+                    MessageBox.Show("Search finished!", "",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Exclamation);
+                    index = 0;
+                }
+            }
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
